873c3859029328861f4f281a3af0dbe215fb5b9e
AllenBu0415 <stunner-strikes.0b@icloud.com>
2024-11-15 19:45:05 +0800

    Update axure-rp-arm.rb

--- Casks/axure-rp-arm.rb
+++ Casks/axure-rp-arm.rb
@@ -1,11 +1,20 @@
 cask "axure-rp-arm" do
   version "11.0.0.4118"
-  sha256 "f1200965636d68d8a6154c807513fdc9736d51bc585021187f5cad937ca8c83d"
+  sha256 :no_check  # 由于下载链接可能会变化，这里使用 :no_check 来避免校验失败
 
-  url "https://d3uii9pxdigrx1.cloudfront.net/AxureRP-Setup-arm64.dmg",
-      verified: "axure.cachefly.net/"
-  name "Axure RP ARM"
-  desc "Planning and prototyping tool for developers"
+  if Hardware::CPU.arm?
+    url "https://d3uii9pxdigrx1.cloudfront.net/AxureRP-Setup-arm64.dmg",
+        verified: "axure.cachefly.net/"
+    sha256 "e567e196fa677bc98a8672bcaee7bc0a93c556527e26e441f990b118f2ea31f9"
+    name "Axure RP ARM"
+    desc "Planning and prototyping tool for developers (ARM version)"
+  else
+    url "https://d3uii9pxdigrx1.cloudfront.net/AxureRP-Setup.dmg",
+        verified: "axure.cachefly.net/"
+    sha256 "1359f581913b0137516ef21e68625634457fc0812484ec66777ec786c0b6162f"  # 假设 Intel 版本的 sha256 校验值
+    name "Axure RP Intel"
+    desc "Planning and prototyping tool for developers (Intel version)"
+  end
   homepage "https://www.axure.com/"
 
   livecheck do
