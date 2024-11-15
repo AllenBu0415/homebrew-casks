40119d16aba669a774c9f0c9b4a2e6eee9497c9b
AllenBu0415 <stunner-strikes.0b@icloud.com>
2024-11-15 17:16:16 +0800

    Create axure-rp-arm.rb

---
+++ axure-rp.rb
@@ -0,0 +1,29 @@
+cask "axure-rp" do
+  version "11.0.0.4118"
+  sha256 "e567e196fa677bc98a8672bcaee7bc0a93c556527e26e441f990b118f2ea31f9"
+
+  url "https://axure.cachefly.net/versions/#{version.csv.first.major_minor.dots_to_hyphens}/AxureRP-Setup-#{version.split(".")[3]}-arm64.dmg",
+      verified: "axure.cachefly.net/"
+  name "Axure RP"
+  desc "Planning and prototyping tool for developers"
+  homepage "https://www.axure.com/"
+
+  livecheck do
+    url "https://www.axure.com/release-history"
+    regex(/>\s*(?:Version|Axure\s*RP)\s*v?(\d+(?:\.\d+)+)/i)
+  end
+
+  depends_on macos: ">= :high_sierra"
+
+  app "Axure RP #{version.major}.app"
+
+  zap trash: [
+    "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.axure.axurerp#*.sfl*",
+    "~/Library/Preferences/com.axure.AxureRP#*.plist",
+    "~/Library/Saved Application State/com.axure.AxureRP#*.savedState",
+  ]
+
+  caveats do
+    requires_rosetta
+  end
+end
